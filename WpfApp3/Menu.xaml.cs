94368c368a597a49e6e5a53cc640a932f7c1fac3
unknown <hamer.tv@outlook.com>
2022-12-01 11:44:02 +0100

    cocs

---
+++ WpfApp3/Menu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void infoP(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("to jest program testujacy menuw klasie 3", "Informacja o programie");
+        }
+    }
+}
